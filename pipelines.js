674f8eeeb33aa503cf1909b438ffbdee437748b4
= <jacex17@gmail.com>
2020-04-07 00:03:37 -0400

    Course finished

--- pipelines.js
+++ pipelines.js
@@ -478,6 +478,276 @@ db.child_reference.aggregate([
 
 
 
+// $graphLookup: Cross Collection Lookup
+db.airlines.aggregate([
+    {$match: {name: "TAP Portugal"} },
+    { $graphLookup: {
+        from: 'routes',
+        as: 'chain',
+        startWith: '$base',
+        connectFromField: 'dst_airport',
+        connectToField: 'src_airport',
+        maxDepth: 1
+    }
+}
+])
+
+// RESTRIC SEARCH
+db.airlines.aggregate([
+    {$match: {name: "TAP Portugal"} },
+    { $graphLookup: {
+        from: 'routes',
+        as: 'chain',
+        startWith: '$base',
+        connectFromField: 'dst_airport',
+        connectToField: 'src_airport',
+        maxDepth: 1,
+        restrictSearchWithMatch: { "airline.name": "TAP Portugal"}
+    }
+}
+])
+
+
+// LAB: $graphLookup
+db.air_alliances.aggregate([
+    {
+      $match: { name: "OneWorld" }
+    },
+    {
+      $graphLookup: {
+        startWith: "$airlines",
+        from: "air_airlines",
+        connectFromField: "name",
+        connectToField: "name",
+        as: "airlines",
+        maxDepth: 0,
+        restrictSearchWithMatch: {
+          country: { $in: ["Germany", "Spain", "Canada"] }
+        }
+      }
+    },
+    {
+      $graphLookup: {
+        startWith: "$airlines.base",
+        from: "air_routes",
+        connectFromField: "dst_airport",
+        connectToField: "src_airport",
+        as: "connections",
+        maxDepth: 1
+      }
+    },
+    {
+      $project: {
+        validAirlines: "$airlines.name",
+        "connections.dst_airport": 1,
+        "connections.airline.name": 1
+      }
+    },
+    { $unwind: "$connections" },
+    {
+      $project: {
+        isValid: {
+          $in: ["$connections.airline.name", "$validAirlines"]
+        },
+        "connections.dst_airport": 1
+      }
+    },
+    { $match: { isValid: true } },
+    {
+      $group: {
+        _id: "$connections.dst_airport"
+      }
+    }
+  ])
+
+//This pipeline takes the most selective collection first, air_alliances, matching the document refering to the OneWorld alliance.
+
+//It then iterates, with maxDepth 0 on the air_airlines collection to collect the details on the airlines, specially their base airport, 
+//but restricting that $lookup to airlines of the requested countries [Spain, Germany, Canada], using restrictSearchWithMatch.
+
+//We then iterate over all routes up to maximum of one layover by setting our maxDepth to 1. 
+//We find all possible destinations when departing from the base airport of each carrier by specify $airlines.base in startWith
+
+//We now have a document with a field named connections that is an array of all routes that are within 1 layover. 
+//We use a $project here to remove unnecessary information from the documents.
+//We also need to include information about valid airlines that match our initial restriction and the name of the current airline.
+
+//After this, we'll unwind our connections array, and then use $project to add a field representing whether this particular route is valid, meaning 
+//it is a route flown by one of our desired carriers.
+
+//Lastly, we use $match to filter out invalid routes, and then $group them on the destination.
+
+//An important aspect to this pipeline is that the first $graphLookup will act as a regular $lookup since we are setting a maxDepth to zero. 
+//The reason why we are taking this approach is due to the match restriction that $graphLookup allows, which can make this stage more efficient. 
+//Think back to the earlier lab on $lookup, can you think of a way to simplify the aggregation using $graphLookup instead?
+
+
+// MULTIDIMENSIONAL GROUPING
+
+// SINGLE FACETS
+db.companies.aggregate([
+    {$match: { '$text': {'$search': 'network'} } },
+    {$unwind: '$offices'},
+    {$match: {'offices.city': {'$ne': ''} } },
+    {$sortByCount: '$offices.city'} // this makes this pipeline a single facet
+])
+
+// MANUAL BUCKETS
+db.companies.aggregate([
+    {
+        $match: {$founded_year: {$gt: 1980} }
+    },
+    {
+        $bucket: {
+            groupBy: $number_of_employees,
+            boundaries: [0, 20, 50, 100, 500, 1000, Infinity], // List values to group By
+            default: 'Other',
+            output: {
+                total: {$sum: 1},
+                average: {$avg: $number_of_employees},
+                categories: {$addToSet: $category_code}
+            }
+        }
+    }
+
+])
+
+// AUTO BUCKETS
+db.companies.aggregate([
+    {
+        $match: {"$offices.city": 'New York'},
+    },
+    {
+        $bucketAuto: {
+            groupBy: $founded_year,
+            buckets: 5
+        }
+    }
+])
+
+//MULTIPLE FACETS
+db.companies.aggregate([
+    {$match: {$text: {$search: 'Databases'} } },
+    {
+        $facet: {
+            'Categories': [{$sortByCount: '$category_code'}],
+            'Employees': [
+                {$match: {'founded_year': {$gt: 1980} } },
+                {$bucket: {
+                    groupBy: '$number_of_employees',
+                    boundaries: [0, 20, 50, 100, 500, 1000, Infinity],
+                    default: 'Other'
+                }}
+            ],
+            'Founded': [
+                {$match: {'offices.city': 'New York'} },
+                {$bucketAuto: {
+                    groupBy: $founded_year,
+                    buckets: 5
+                }}
+            ]
+        }
+    }
+])
+
+
+//
+
+db.movies.aggregate([
+    {
+      $match: { metacritic: { $gte: 0 }, "imdb.rating": { $gte: 0 } }
+    },
+    {
+      $project: { _id: 0, metacritic: 1, imdb: 1, title: 1 }
+    },
+    {
+      $facet: {
+        top_metacritic: [
+          {
+            $sort: { metacritic: -1, title: 1 }
+          },
+          {
+            $limit: 10
+          },
+          {
+            $project: { title: 1 }
+          }
+        ],
+        top_imdb: [
+          {
+            $sort: { "imdb.rating": -1, title: 1 }
+          },
+          {
+            $limit: 10
+          },
+          {
+            $project: { title: 1 }
+          }
+        ]
+      }
+    },
+    {
+      $project: {
+        movies_in_both: {
+          $setIntersection: ["$top_metacritic", "$top_imdb"]
+        }
+      }
+    }
+  ])
+
+
+// EXERCISE
+
+  //Using the air_alliances and air_routes collections, find which alliance has the most unique carriers(airlines) operating between the airports JFK and LHR, 
+  // in either directions.
+
+  //Names are distinct, i.e. Delta != Delta Air Lines
+  
+  // src_airport and dst_airport contain the originating and terminating airport information.  
+
+db.air_routes.aggregate([
+    {
+      $match: {
+        src_airport: { $in: ["LHR", "JFK"] },
+        dst_airport: { $in: ["LHR", "JFK"] }
+      }
+    },
+    {
+      $lookup: {
+        from: "air_alliances",
+        foreignField: "airlines",
+        localField: "airline.name",
+        as: "alliance"
+      }
+    },
+    {
+      $match: { alliance: { $ne: [] } }
+    },
+    {
+      $addFields: {
+        alliance: { $arrayElemAt: ["$alliance.name", 0] }
+      }
+    },
+    {
+      $group: {
+        _id: "$airline.id",
+        alliance: { $first: "$alliance" }
+      }
+    },
+    {
+      $sortByCount: "$alliance"
+    }
+  ])
+
+  // We begin with a $match stage and fetch routes that originate or end at either LHR and JFK
+
+  // We then $lookup into the air_alliances collection, matching member airline names in the airlines field to the local airline.name field in the route
+
+  // We follow with a $match stage to remove routes that are not members of an alliance. 
+  // We use $addFields to cast just the name of the alliance and extract a single element in one go
+
+  // Lastly, we $group on the airline.id, since we don't want to count the same airline twice. We take the $first alliance name to avoid duplicates. 
+  // Then, we use $sortByCount to get our answer from the results
 
 
 
@@ -492,6 +762,26 @@ db.child_reference.aggregate([
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//We begin with a $match and $project stage to only look at documents with the relevant fields, and project away needless information
+
+// Next follows our $facet stage.
+// Within each facet, we need sort in descending order for metacritic and imdb.ratting and ascending for title, limit to 10 documents, then only retain the title
+
+// Lastly, we use a $project stage to find the intersection of top_metacritic and top_imdb, producing the titles of movies in both categories
 
 
 
